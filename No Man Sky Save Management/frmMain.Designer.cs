2ee56eb59cba30fc3082196e0265949843ad51bc
Matias A.V <drakth@users.noreply.github.com>
2021-05-24 22:17:37 -0300

    Initial Commit
    Initial Commit

---
+++ No Man Sky Save Management/frmMain.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace No_Man_Sky_Save_Management
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FSW = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            this.dlgFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDelay = new System.Windows.Forms.TextBox();
+            this.cmdEnable = new System.Windows.Forms.Button();
+            this.cmdDisable = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDestination = new System.Windows.Forms.TextBox();
+            this.cmdBrowseDest = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.chkRemember = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.FSW)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FSW
+            // 
+            this.FSW.EnableRaisingEvents = true;
+            this.FSW.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(447, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NMS Folder (Ex: C:\\Users\\username\\AppData\\Roaming\\HelloGames\\NMS\\st_1234567890)";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(12, 25);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(540, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.Location = new System.Drawing.Point(558, 22);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(26, 23);
+            this.cmdBrowse.TabIndex = 2;
+            this.cmdBrowse.Text = "...";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Delay after change (Seconds)";
+            // 
+            // txtDelay
+            // 
+            this.txtDelay.Location = new System.Drawing.Point(12, 64);
+            this.txtDelay.Name = "txtDelay";
+            this.txtDelay.Size = new System.Drawing.Size(42, 20);
+            this.txtDelay.TabIndex = 4;
+            this.txtDelay.Text = "1";
+            // 
+            // cmdEnable
+            // 
+            this.cmdEnable.Location = new System.Drawing.Point(12, 127);
+            this.cmdEnable.Name = "cmdEnable";
+            this.cmdEnable.Size = new System.Drawing.Size(103, 23);
+            this.cmdEnable.TabIndex = 5;
+            this.cmdEnable.Text = "Enable Monitoring";
+            this.cmdEnable.UseVisualStyleBackColor = true;
+            this.cmdEnable.Click += new System.EventHandler(this.cmdEnable_Click);
+            // 
+            // cmdDisable
+            // 
+            this.cmdDisable.Enabled = false;
+            this.cmdDisable.Location = new System.Drawing.Point(121, 127);
+            this.cmdDisable.Name = "cmdDisable";
+            this.cmdDisable.Size = new System.Drawing.Size(103, 23);
+            this.cmdDisable.TabIndex = 6;
+            this.cmdDisable.Text = "Disable Monitoring";
+            this.cmdDisable.UseVisualStyleBackColor = true;
+            this.cmdDisable.Click += new System.EventHandler(this.cmdDisable_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Destination Folder";
+            // 
+            // txtDestination
+            // 
+            this.txtDestination.Location = new System.Drawing.Point(12, 103);
+            this.txtDestination.Name = "txtDestination";
+            this.txtDestination.ReadOnly = true;
+            this.txtDestination.Size = new System.Drawing.Size(540, 20);
+            this.txtDestination.TabIndex = 8;
+            // 
+            // cmdBrowseDest
+            // 
+            this.cmdBrowseDest.Location = new System.Drawing.Point(558, 100);
+            this.cmdBrowseDest.Name = "cmdBrowseDest";
+            this.cmdBrowseDest.Size = new System.Drawing.Size(26, 23);
+            this.cmdBrowseDest.TabIndex = 9;
+            this.cmdBrowseDest.Text = "...";
+            this.cmdBrowseDest.UseVisualStyleBackColor = true;
+            this.cmdBrowseDest.Click += new System.EventHandler(this.cmdBrowseDest_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Log";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(12, 169);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(572, 218);
+            this.txtLog.TabIndex = 11;
+            // 
+            // chkRemember
+            // 
+            this.chkRemember.AutoSize = true;
+            this.chkRemember.Location = new System.Drawing.Point(230, 133);
+            this.chkRemember.Name = "chkRemember";
+            this.chkRemember.Size = new System.Drawing.Size(107, 17);
+            this.chkRemember.TabIndex = 12;
+            this.chkRemember.Text = "Remember Paths";
+            this.chkRemember.UseVisualStyleBackColor = true;
+            this.chkRemember.Click += new System.EventHandler(this.chkRemember_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 398);
+            this.Controls.Add(this.chkRemember);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmdBrowseDest);
+            this.Controls.Add(this.txtDestination);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmdDisable);
+            this.Controls.Add(this.cmdEnable);
+            this.Controls.Add(this.txtDelay);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmdBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "No Man Sky Backup Savegame";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.FSW)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher FSW;
+        private System.Windows.Forms.Button cmdBrowse;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolder;
+        private System.Windows.Forms.TextBox txtDelay;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmdDisable;
+        private System.Windows.Forms.Button cmdEnable;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button cmdBrowseDest;
+        private System.Windows.Forms.TextBox txtDestination;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkRemember;
+    }
+}
+
---
+++ No Man Sky Save Management/frmMain.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/* No Man Sky Savegame Backup, was born because i was annoyed with the bug
+ * about the expeditions ending prematurly, so I decided to make a small 
+ * utility to make backups of the savegames as changes are detected.
+ * 
+    Copyright(C) 2021  Matias A. Villagarcia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<https://www.gnu.org/licenses/>.
+*/
+
+namespace No_Man_Sky_Save_Management
+{
+    public partial class frmMain : Form
+    {
+        string destination = "";
+        public frmMain()
+        {
+            InitializeComponent();
+
+            //Create the callbacks for the File System Watcher object.
+            FSW.Created += fsw_created;
+            FSW.Changed += fsw_changed;
+            FSW.Deleted += fsw_deleted;
+        }
+
+        private void enableFSWEvents()
+        {
+            FSW.EnableRaisingEvents = true;
+        }
+
+        private void disableFSWEvents()
+        {
+            FSW.EnableRaisingEvents = false;
+        }
+
+        private void cmdBrowse_Click(object sender, EventArgs e)
+        {
+            dlgFolder.ShowDialog();
+            txtPath.Text = dlgFolder.SelectedPath;
+            txtLog.AppendText(DateTime.Now + " NMS save folder set." + Environment.NewLine);
+        }
+
+        private void cmdBrowseDest_Click(object sender, EventArgs e)
+        {
+            dlgFolder.ShowDialog();
+            txtDestination.Text = dlgFolder.SelectedPath;
+            txtLog.AppendText(DateTime.Now + " Destination folder set." + Environment.NewLine);
+        }
+
+        private void cmdEnable_Click(object sender, EventArgs e)
+        {
+            //Set up the callbacks and start monitoring for changes.
+            try
+            {
+                string rutaNMS = txtPath.Text;
+
+                if (rutaNMS != "")
+                {
+                    if(txtDestination.Text != "" )
+                    {
+                        FSW.Path = rutaNMS;
+
+                        enableFSWEvents();
+                        cmdEnable.Enabled = false;
+                        cmdDisable.Enabled = true;
+                        txtLog.AppendText(DateTime.Now + " Monitoring enabled!" + Environment.NewLine);
+                    }else
+                    {
+                        txtLog.AppendText(DateTime.Now + " Invalid destination folder." + Environment.NewLine);
+                    }
+                }else
+                {
+                    txtLog.AppendText(DateTime.Now + " Invalid NMS path." + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        //TODO: Instead of duplicating code in create and modify, we should probably modularize it.
+
+        private void fsw_created (object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                int delay = Convert.ToInt32(txtDelay.Text);
+                if (e.Name != "steam_autocloud.vdf") //We dont want to process changes to steam cloud files.
+                {
+                    Task.Delay(Convert.ToInt32(txtDelay.Text) * 1000).Wait(); //If we do not wait the file will still be used by NMS and give an error.
+
+                    //We disable monitoring so we dont process further changes until
+                    //we are done copying files.
+                    disableFSWEvents();
+                    txtLog.AppendText(DateTime.Now + " " + e.FullPath + " Changed." + Environment.NewLine);
+
+                    //Check if destination folder exists, if it does not create it and copy the files.
+                    destination = Convert.ToString(DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year + "-" + DateTime.Now.Hour + "." + DateTime.Now.Minute);
+                    destination = destination.Replace("/", "-");
+                    destination = destination.Replace(":", ".");
+                    if (Directory.Exists(txtDestination.Text + "\\" + destination) == false)
+                    {
+                        Directory.CreateDirectory(txtDestination.Text + "\\" + destination);
+                        File.Copy(e.FullPath, txtDestination.Text + "\\" + destination + "\\" + e.Name, true);
+                    }
+                    else
+                    {
+                        File.Copy(e.FullPath, txtDestination.Text + "\\" + destination + "\\" + e.Name, true);
+                    }
+                    txtLog.AppendText(DateTime.Now + " " + e.Name + " File copied." + Environment.NewLine);
+
+                    enableFSWEvents();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void fsw_changed(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                int delay = Convert.ToInt32(txtDelay.Text);
+                if (e.Name != "steam_autocloud.vdf") //We dont want to process changes to steam cloud files.
+                {
+                    Task.Delay(Convert.ToInt32(txtDelay.Text) * 1000).Wait(); //If we do not wait the file will still be used by NMS and give an error.
+
+                    //We disable monitoring so we dont process further changes until
+                    //we are done copying files.
+                    disableFSWEvents();
+                    txtLog.AppendText(DateTime.Now + " " + e.FullPath + " Changed." + Environment.NewLine);
+
+                    //Check if destination folder exists, if it does not create it and copy the files.
+                    destination = Convert.ToString(DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year + "-" + DateTime.Now.Hour + "." + DateTime.Now.Minute);
+                    destination = destination.Replace("/", "-");
+                    destination = destination.Replace(":", ".");
+                    if (Directory.Exists(txtDestination.Text + "\\" + destination) == false)
+                    {
+                        Directory.CreateDirectory(txtDestination.Text + "\\" + destination);
+                        File.Copy(e.FullPath, txtDestination.Text + "\\" + destination + "\\" + e.Name, true);
+                    }
+                    else
+                    {
+                        File.Copy(e.FullPath, txtDestination.Text + "\\" + destination + "\\" + e.Name, true);
+                    }
+                    txtLog.AppendText(DateTime.Now + " " + e.Name + " File copied." + Environment.NewLine);
+
+                    enableFSWEvents();
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void fsw_deleted (object sender, FileSystemEventArgs e)
+        {
+            //Not sure if this will be used?
+        }
+
+        private void cmdDisable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                disableFSWEvents();
+                cmdEnable.Enabled = true;
+                cmdDisable.Enabled = false;
+                txtLog.AppendText(DateTime.Now + " Monitoring Disabled!" + Environment.NewLine);
+            }
+            catch(Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void chkRemember_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (chkRemember.Checked == true)
+                {
+                    if (txtPath.Text != "" && txtDestination.Text != "")
+                    {
+                        StreamWriter sw = new StreamWriter(Application.StartupPath + "\\paths.txt", false);
+                        sw.WriteLine(txtPath.Text);
+                        sw.WriteLine(txtDestination.Text);
+                        sw.Close();
+                    }
+                    else
+                    {
+                        txtLog.AppendText(DateTime.Now + " Both paths must be completed." + Environment.NewLine);
+                        chkRemember.Checked = false;
+                    }
+                }
+                else
+                {
+                    if(File.Exists(Application.StartupPath + "\\paths.txt"))
+                    {
+                        File.Delete(Application.StartupPath + "\\paths.txt");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if(File.Exists(Application.StartupPath + "\\paths.txt"))
+                {
+                    StreamReader sr = new StreamReader(Application.StartupPath + "\\paths.txt");
+                    txtPath.Text = sr.ReadLine();
+                    txtDestination.Text = sr.ReadLine();
+                    sr.Close();
+                    chkRemember.Checked = true;
+                }else
+                {
+                    chkRemember.Checked = false;
+                }
+            }catch(Exception ex)
+            {
+                txtLog.AppendText(DateTime.Now + " ERROR: " + ex.Message + Environment.NewLine);
+            }
+        }
+    }
+}
